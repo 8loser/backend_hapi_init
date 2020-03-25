709d494005dbde2099b5efb424fcec9e136ee1bd
8loser@gmail.com <8loser@gmail.com>
2020-03-25 22:17:43 +0800

    index.js

--- index.js
+++ index.js
@@ -2,27 +2,24 @@
 
 const Hapi = require('@hapi/hapi');
 
-//載入全域設定檔 .env
+// 載入全域設定檔 .env
 require('dotenv').config()
 
-//資料庫位址
+// 使用MongoDB資料庫
+// 資料庫位址
 const MongoDBUrl = process.env.DBUrl;
+
+//路徑讀取routes內檔案
+const routes = require('./src/routes');
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: process.env.SrvPort,
+        port: process.env.ServerPort,
         host: 'localhost'
     });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
-        }
-    });
+    server.route(routes);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
@@ -32,8 +29,5 @@ process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
-
-//載入所有route
-const routes = require('./src/routes');
 
 init();
